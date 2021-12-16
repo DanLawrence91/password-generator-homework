a978c80ce8018f40137e85c642e2cfc17298b20e
Daniel Lawrence <dan.lawrence0810@gmail.com>
2021-12-16 22:50:12 +1030

    1st prompt for length started

--- script.js
+++ script.js
@@ -10,15 +10,31 @@ function writePassword() {
 
 }
 
+// variables set for different character types
+var charsCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var charsNumeric = "1234567890"
+var charsLower = "abcdefghijklmnopqrstuvwxyz"
+var special = "!@#$%^&*();:,.<>"
 // set function for generate password to include prompts from acceptance criteria
-function generatePassword() {
-  // variables set for different character types
-  var charsCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var charsNumeric = "1234567890"
-  var charsLower = "abcdefghijklmnopqrstuvwxyz"
-  var special = "!@#$%^&*();:,.<>"
-
+var generatePassword = function() {
+  
   //prompts needed for each character type and password length
+  var charNum = prompt("Please pick how many characters you want the password to be. It must be between 8 and 128 characters in length");
+
+  if (!charNum){
+    return;
+  }
+
+  //need to test if this set to 7 or 8. also may need to change from confirm to other e.g prompt or alert
+  if (charNum < 8){
+    return confirm("Please select number of characters over 8");
+  }
+
+  //need to test if this set to 127 or 128. also may need to change from confirm to other e.g prompt or alert
+  if (charNum > 128){
+    return confirm("Please select number of characters below 128");
+  }
+
   
 }
 
